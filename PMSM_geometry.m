a9ef26d016786e34c2ef8cfadb7089b600b96e10
vivienwolive-wq <vivien.w.olive@gmail.com>
2025-10-27 11:21:24 +0100

    Revise parameters for permanent magnet machine model
    Updated geometric parameters and characteristics for the permanent magnet machine, including changes to slot numbers, dimensions, and connectivity matrix.

--- PMSM_geometry.m
+++ PMSM_geometry.m
@@ -31,7 +31,7 @@ scl=3;
 % ---------------------------------------
         Npart=1; % We are studiying a complete machine (keep 1)
 % 	-- Slot number 
-		N_slo_s=30;  % From Excel: 24 slots
+		N_slo_s=48;  % From Excel: 24 slots
 % 	-- Tooth pitch
       op_t_s=2*pi/Npart/N_slo_s;        % in rad
 
@@ -44,8 +44,8 @@ scl=3;
 	w2_s =  10 ;  % --Low side slot opening in mm (your selected width)
 	w3_s =  15;  % --High side slot opening in mm
 
-	h1_s  =  2 ;  % --Isthmus height in mm
-	h2_s  =  3 ;  % --Base height in mm
+	h1_s  =  3 ;  % --Isthmus height in mm
+	h2_s  =  5 ;  % --Base height in mm
 	h3_s  =  25; % --Main height in mm (your selected total height = 29.5 mm)
                                          
     % Slot surface area calculation
@@ -58,7 +58,7 @@ scl=3;
    Npoles =4; % Number of poles - From Excel: 4 poles
    thi_mag=3.4; % Thickness of the magnets - From Excel: 3.4 mm
    p=Npoles/2; % Number of pairs of poles
-   Nseg  =6;   % Number of magnet segmentations 
+   Nseg  =10;   % Number of magnet segmentations 
    % Percentage of magnet over a pole pitch in degres and in rad
    op_mag_deg=80 ;    op_mag=op_mag_deg*pi/180;  % From Excel: 80° magnet coverage
    
@@ -67,19 +67,21 @@ scl=3;
 % -- Radius
 % ------------------------------
 	Re   = 300.0;  % Max radius = 300mm (600mm diameter / 2)
-	Ra   = 350/2;    % Bore radius = 225mm (450mm diameter / 2)
-    airgap=3;    % From Excel: 0.8 mm air gap
+	Ra   = 400/2;    % Bore radius = 225mm (450mm diameter / 2)
+    airgap=5;    % From Excel: 0.8 mm air gap
 	Rr   = Ra-airgap-thi_mag;  % Rotor radius
-    Ri   = 100.0;  % Inner radius
+    Ri   = 50.0;  % Inner radius
 
-Nturns=8;  % From Excel: 11 turns per coil
+Nturns=4;  % From Excel: 11 turns per coil
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Connectivity matrix
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
    
-     Mat_C1=[2 2 1 0 0 0 0 -1 -2 -2 0 0 0 0 0 2 2 1 0 0 0 0 -1 -2 -2 0 0 0 0 0
-               0 0 0 0 0 2 2 1 0 0 0 0 -1 -2 -2 0 0 0 0 0 2 2 1 0 0 0 0 -1 -2 -2
-               0 0 -1 -2 -2 0 0 0 0 0 2 2 1 0 0 0 0 -1 -2 -2 0 0 0 0 0 2 2 1 0 0
+   
+
+    Mat_C1=[2 2 1 1 0 0 0 0 0 0 -1 -1 -2 -2 -1 -1 0 0 0 0 0 0 1 1
+               0 0 0 0 0 0 1 1 2 2 1 1 0 0 0 0 0 0 -1 -1 -2 -2 -1 -1
+               0 0 -1 -1 -2 -2 -1 -1 0 0 0 0 0 0 1 1 2 2 1 1 0 0 0 0
 ];
-Mat_C=[Mat_C1];
+Mat_C=[Mat_C1 Mat_C1];
